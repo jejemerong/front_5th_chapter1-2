97b07400c2671af0bd619d56e5a93ab6b60c0d82
jejemerong <9708jjw@gmail.com>
2025-03-31 16:27:34 +0900

    feat: normalizeVNode 수정

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,30 @@
+const isNotFalsy = (el) =>
+  el !== null && el !== undefined && typeof el !== "boolean";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (!isNotFalsy(vNode)) {
+    return "";
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const { type, props, children } = vNode;
+    // 컴포넌트 호출
+    const component = type({
+      ...props, // 불변성
+      children,
+    });
+    return normalizeVNode(component); // 재귀
+  }
+
+  // children 정리
+  return {
+    ...vNode,
+    children: vNode.children
+      ? vNode.children.filter(isNotFalsy).map(normalizeVNode) // 자식 노드
+      : [], // undefined 일 경우 렌더링 제외
+  };
 }
