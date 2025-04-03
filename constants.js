3a5bbabe2a3960f56dabcc0b8bbb3452beb2d8fe
jejemerong <9708jjw@gmail.com>
2025-04-03 23:33:00 +0900

    fix: 배포 경로 설정

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const REPO_URL = "/front_5th_chapter1-2";
--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -1,14 +1,25 @@
+import { REPO_URL } from "../../constants";
 import { createObserver } from "./createObserver";
 
 export const createRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  const getPath = () => window.location.pathname;
+  const getPath = () => {
+    const baseUrl = import.meta.env.MODE === "production" ? REPO_URL : "";
+    return window.location.pathname.replace(baseUrl, "") || "/";
+  };
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
-    window.history.pushState(null, null, path);
+    if (import.meta.env.MODE === "production") {
+      const baseUrl = import.meta.env.MODE === "production" ? REPO_URL : "";
+      const fullPath = baseUrl + path;
+
+      window.history.pushState(null, null, fullPath);
+    } else {
+      window.history.pushState(null, null, path);
+    }
     notify();
   };
 
