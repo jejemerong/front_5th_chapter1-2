34278199259f43811d75907d133e54d5701c488f
jejemerong <9708jjw@gmail.com>
2025-04-03 18:33:13 +0900

    feat: 포스트 제출 함수 추가

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,29 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
 export const PostForm = () => {
+  const { posts, currentUser } = globalStore.getState();
+
+  function onSubmitPost(e) {
+    e.preventDefault();
+    const content = document.getElementById("post-content").value;
+    const author = currentUser.username;
+
+    globalStore.setState({
+      posts: [
+        ...posts,
+        {
+          id: posts.length + 1,
+          author,
+          time: new Date(),
+          content,
+          likeUsers: [],
+        },
+      ],
+    });
+  }
+
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -12,6 +34,7 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onclick={onSubmitPost}
       >
         게시
       </button>
